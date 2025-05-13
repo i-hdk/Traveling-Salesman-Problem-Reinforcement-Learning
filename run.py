9eef3a0f637e5352a14168a7ff8a0849113ac56b
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2025-05-13 14:27:05 +0800

    add random vs trained agent bar graph

--- run.py
+++ run.py
@@ -32,11 +32,12 @@ class RewardTrackerCallback(BaseCallback):
         plt.title("Training Reward Over Time")
         plt.legend()
         plt.grid(True)
-        plt.show()
+        plt.show(block=False)
 
 #TODO add greedy policy
 
 def evaluate_random_policy(env, episodes=3):
+    random_avg_total_dist = 0
     for ep in range(episodes):
         obs, info = env.reset()
         done = False
@@ -63,8 +64,12 @@ def evaluate_random_policy(env, episodes=3):
             env.render()
             done = terminated or truncated
         print(f"Finished tour. Total Distance: {info['total_distance']:.2f}")
+        random_avg_total_dist+=info['total_distance']
+    random_avg_total_dist/=episodes
+    return random_avg_total_dist
 
 def evaluate_trained_agent(env, model, episodes=3):
+    trained_avg_total_dist = 0
     for ep in range(episodes):
         obs, info = env.reset()
         done = False
@@ -76,8 +81,11 @@ def evaluate_trained_agent(env, model, episodes=3):
             env.render()
             done = terminated or truncated
         print(f"Finished tour. Total Distance: {info['total_distance']:.2f}")
+        trained_avg_total_dist+=info['total_distance']
+    trained_avg_total_dist/=episodes
+    return trained_avg_total_dist
 
-
+print("running")
 env = TSPEnv.TSPEnv(num_cities=5)
 check_env(env)
 model = PPO("MultiInputPolicy", env, verbose=1) #TODO try different policies or models
@@ -87,10 +95,23 @@ callback.plot_rewards()
 
 #TODO add save/load model with a separate file to separate training & testing 
 
-evaluate_random_policy(env)
-evaluate_trained_agent(env, model)
+random_avg_total_dist = evaluate_random_policy(env,episodes=10)
+trained_avg_total_dist = evaluate_trained_agent(env, model,episodes=10)
 
 #TODO add bar graphs with random / greedy / different trained models on avg total dist travelled
+print(f"random policy average total distance: {random_avg_total_dist:.3f}")
+print(f"trained agent average total distance: {trained_avg_total_dist:.3f}")
+plt.figure()
+categories = ["Random Policy", "Trained Agent"]
+values = [random_avg_total_dist, trained_avg_total_dist]
+plt.bar(categories, values)
+#plt.xlabel("Categories")
+plt.ylabel("Total Distance Average")
+plt.title("Total Distance Averages for Different Policies")
+plt.show(block=False)
 
 env.close()
+
+input("Press Enter to exit...")
+
 
